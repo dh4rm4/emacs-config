8a9318b570c0980c962649f53938cc6d30e60d0b
dh4rm4 <dh4rm4@protonmail.com>
2019-07-09 13:54:44 +0200

    add switch to remote ssh link

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 sudo apt install -yy emacs
+
 rm -Rf ${HOME}/.emacs ${HOME}/.emacs.d/custom_confs
+
 ln -s $PWD/emacs.el ${HOME}/.emacs
 ln -s $PWD/custom_confs ${HOME}/.emacs.d/custom_confs
+
+git remote set-url origin git@gitlab.com:posedao/emacs-config.git
