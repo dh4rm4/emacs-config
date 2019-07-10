e5c671b9c3def97de2bc55860069d398d625fc6e
喜 <dh4rm4@protonmail.com>
2019-07-10 16:14:23 +0000

    Update shortcuts.el

--- custom_confs/shortcuts.el
+++ custom_confs/shortcuts.el
@@ -3,3 +3,35 @@
 
 ;; --- replace killing all buffer, by just the current one
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
+
+
+;; --- WINDOWS ACTIONS ---                                                                                
+;; --- Delete current window                                                                              
+(global-set-key (kbd "C-x w") 'delete-window)
+
+;; --- Spliter                                                                                            
+(defun empty-current-window ()
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf
+    ))
+
+(defun split-vertically ()
+  (interactive)
+  (split-window-right)
+  (windmove-right)
+  (empty-current-window)
+  )
+
+(defun split-horizontally ()
+  (interactive)
+  (split-window-below)
+  (windmove-down)
+  (empty-current-window)
+  )
+
+(global-set-key (kbd "C-M-@") 'split-vertically)
+(global-set-key (kbd "M-SPC") 'split-horizontally)
