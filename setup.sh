86a5d5adb24f0ef66bc1f9587f6f5c4d67653bfd
dh4rm4 <dh4rm4@protonmail.com>
2019-09-01 09:35:30 +0200

    remove .myemacs file via script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt install -yy emacs
 
-rm -Rf ${HOME}/.emacs ${HOME}/.emacs.d/custom_confs
+rm -Rf ${HOME}/.emacs ${HOME}/.emacs.d/custom_confs ${HOME}/.myemacs
 
 ln -s $PWD/emacs.el ${HOME}/.emacs
 ln -s $PWD/custom_confs ${HOME}/.emacs.d/custom_confs
